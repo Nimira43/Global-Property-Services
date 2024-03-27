517c0a78e730fe9f373e452b27c7c5070b1cb5ce
Nick Rabone <n.rabone72@aol.com>
2024-03-27 22:38:39 +0000

    5th - Project reset after previous tutorial - that tutorial went indepth about server and client components.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,21 +1,8 @@
 'use client';
-
-import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
-
 const PropertyPage = () => {
-  
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const { id } = useParams();
-  const name = searchParams.get('name');
-  const pathname = usePathname();
-
-  console.log('hi');
-
   return (
     <div>
-      <h1>Property Page</h1>
-      <button onClick={() => router.push('/')} className='bg-red-500 p-2'>Home { pathname }</button>
+        Property Page
     </div>
   )
 };
