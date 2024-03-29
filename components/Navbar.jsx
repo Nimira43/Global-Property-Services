28aaf78a81368a4943d5a64459496f87b1ae0cf4
Nick Rabone <n.rabone72@aol.com>
2024-03-29 22:22:36 +0000

    Updated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,17 @@
+import Image from 'next/image';
+// import logo from '@/assets/images/logo-white.png';
+import profileDefault from '@/assets/images/profile.png';
+
 const Navbar = () => {
     return (
-        <nav className="bg-yellow-500 border-b border-black">
+        <nav className="bg-grey-100 border-b border-black">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-20 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                         <button
                             type="button"
                             id="mobile-dropdown-button"
-                            className="relative inline-flex items-center justify-center rounded-md p-2 text-red-700 hover:text-black"
+                            className="relative inline-flex items-center justify-center rounded-md p-2 text-blue-500 hover:text-black"
                             aria-controls="mobile-menu"
                             aria-expanded="false"
                         >
@@ -17,13 +21,13 @@ const Navbar = () => {
                                 className="block h-6 w-6"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                stroke-width="1.5"
+                                strokeWidth="1.5"
                                 stroke="currentColor"
                                 aria-hidden="true"
                             >
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                                 />
                             </svg>
@@ -34,7 +38,7 @@ const Navbar = () => {
                         className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
                     >
                         <a className="flex flex-shrink-0 items-center" href="/index.html">
-                            <span className="hidden md:block text-red-700 text-2xl font-bold ml-2"
+                            <span className="hidden md:block text-blue-500 text-2xl font-bold ml-2"
                             >Global Property Services</span
                             >
                         </a>
@@ -42,17 +46,17 @@ const Navbar = () => {
                             <div className="flex space-x-2">
                                 <a
                                     href="/index.html"
-                                    className="text-black hover:text-red-700 px-2 py-1"
+                                    className="text-black hover:text-blue-500 px-2 py-1"
                                 >Home</a
                                 >
                                 <a
                                     href="/properties.html"
-                                    className="text-black hover:text-red-700 px-2 py-1"
+                                    className="text-black hover:text-blue-500 px-2 py-1"
                                 >Properties</a
                                 >
                                 <a
                                     href="/add-property.html"
-                                    className="text-black hover:text-red-700 px-2 py-1"
+                                    className="text-black hover:text-blue-500 px-2 py-1"
                                 >Add Property</a
                                 >
                             </div>
@@ -62,9 +66,9 @@ const Navbar = () => {
                     <div className="hidden md:block md:ml-6">
                         <div className="flex items-center">
                             <button
-                                className="flex items-center text-black hover:text-red-700 rounded-md px-2 py-1"
+                                className="flex items-center text-black hover:text-blue-500 rounded-md px-2 py-1"
                             >
-                                <i className="fa-brands fa-google text-yellow-500 mr-2"></i>
+                                <i className="fa-brands fa-google text-white mr-2"></i>
                                 <span>Login or Register</span>
                             </button>
                         </div>
@@ -76,7 +80,7 @@ const Navbar = () => {
                         <a href="messages.html" className="relative group">
                             <button
                                 type="button"
-                                className="relative rounded-full bg-red-700 p-1 text-yellow-500 hover:bg-black"
+                                className="relative rounded-full bg-black p-1 text-white hover:bg-blue-500"
                             >
                                 <span className="absolute -inset-1.5"></span>
                                 <span className="sr-only">View notifications</span>
@@ -84,19 +88,19 @@ const Navbar = () => {
                                     className="h-6 w-6"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    stroke-width="1.5"
+                                    strokWidth="1.5"
                                     stroke="currentColor"
                                     aria-hidden="true"
                                 >
                                     <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
                                     />
                                 </svg>
                             </button>
                             <span
-                                className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-700 transform translate-x-1/2 -translate-y-1/2 bg-black rounded-full"
+                                className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-blue-500 rounded-full"
                             >
                                 2
                             </span>
@@ -105,16 +109,16 @@ const Navbar = () => {
                             <div>
                                 <button
                                     type="button"
-                                    className="relative flex rounded-full bg-red-700 text-sm"
+                                    className="relative flex rounded-full bg-black text-sm"
                                     id="user-menu-button"
                                     aria-expanded="false"
                                     aria-haspopup="true"
                                 >
                                     <span className="absolute -inset-1.5"></span>
                                     <span className="sr-only">Open user menu</span>
-                                    <img
+                                    <Image
                                         className="h-8 w-8 rounded-full"
-                                        src="images/profile.png"
+                                        src={profileDefault}
                                         alt=""
                                     />
                                 </button>
@@ -126,13 +130,13 @@ const Navbar = () => {
                                 role="menu"
                                 aria-orientation="vertical"
                                 aria-labelledby="user-menu-button"
-                                tabindex="-1"
+                                tabIndex="-1"
                             >
                                 <a
                                     href="/profile.html"
                                     className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-0"
                                 >Your Profile</a
                                 >
@@ -140,7 +144,7 @@ const Navbar = () => {
                                     href="saved-properties.html"
                                     className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-2"
                                 >Saved Properties</a
                                 >
@@ -148,7 +152,7 @@ const Navbar = () => {
                                     href="#"
                                     className="block px-4 py-2 text-sm text-gray-700"
                                     role="menuitem"
-                                    tabindex="-1"
+                                    tabIndex="-1"
                                     id="user-menu-item-2"
                                 >Sign Out</a
                                 >
